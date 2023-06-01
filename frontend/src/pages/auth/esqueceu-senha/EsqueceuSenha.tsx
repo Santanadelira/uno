9294298a96631047af25ba0b1882c75673bd9e45
Santanadelira <125101830+Santanadelira@users.noreply.github.com>
2023-06-01 13:52:36 -0300

    FRONTEND - Tela esqueceu a senha

--- frontend/src/pages/auth/esqueceu-senha/EsqueceuSenha.tsx
+++ frontend/src/pages/auth/esqueceu-senha/EsqueceuSenha.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { useFormik } from "formik";
+import React from "react";
+import logo from "../../../assets/logo.svg";
+import * as yup from "yup";
+import { RxArrowLeft } from "react-icons/rx";
+import { Link } from "react-router-dom";
 
-const EsqueceuSenha = () => {
+const esqueceuSenha: React.FC = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+
+    validationSchema: yup.object().shape({
+      email: yup
+        .string()
+        .email("Email inválido!")
+        .required("Campo obrigatório!"),
+    }),
+
+    onSubmit: async (values) => {
+      //empty for now
+    },
+  });
+
   return (
-    <div>EsqueceuSenha</div>
-  )
-}
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="w-4/5 sm:w-96">
+        <Link
+          to="/login"
+          className="text-indigo-600 hover:text-indigo-800 flex items-center gap-2 mb-7"
+        >
+          <RxArrowLeft size={15} />
+          <p className="text-xs font-lato">Voltar ao login</p>
+        </Link>
+        <img src={logo} alt="logo" className="h-10 mx-auto" />
+        <h3 className="font-lato text-2xl text-gray-800 text-center my-7 font-black">
+          Esqueceu senha?
+        </h3>
+        <p className="text-grey-700 font-lato text-sm text-center mb-7">
+          Informe seu email abaixo e enviaremos as instruções de recuperação
+          pelo email!
+        </p>
 
-export default EsqueceuSenha
+        <form>
+          <div className="mb-7">
+            <label
+              htmlFor="email"
+              className="text-gray-900 text-sm font-lato mb-1"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              className="text-sm py-2 px-3 rounded-md shadow-sm ring-1 ring-gray-300 ring-inset focus:ring-2 focus:ring-inset focus:outline-none w-full focus:ring-indigo-600"
+            />
+          </div>
+
+          <button
+            className="bg-indigo-600 w-full py-1.5 rounded-md text-white font-lato font-medium hover:bg-indigo-700"
+            type="submit"
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default esqueceuSenha;

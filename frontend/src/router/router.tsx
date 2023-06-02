923396bc8f7b6950cafddecfd2878bce40d078cf
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-06-02 11:33:11 -0300

    FRONTEND

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Login from "../pages/auth/login/Login";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
+            {
+                path: "/login",
+                element: <Login />
+            }
         ]
     }
 ])

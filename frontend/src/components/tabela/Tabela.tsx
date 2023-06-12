bdb425ff93b94165daaf56c9a39454634228c4cf
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-06-12 14:02:31 -0300

    PI

--- frontend/src/components/tabela/Tabela.tsx
+++ frontend/src/components/tabela/Tabela.tsx
@@ -148,7 +148,7 @@ const Tabela = ({ titulo, colunas, dados, textoPesquisa, consultarRota, editar, 
                               editar && <Menu.Item>
                               {({ active }) => (
                                 <Link
-                                  to={`${consultarRota}/${dado.id}/editar`}
+                                  to={`${consultarRota}/${dado.itemRota}/editar`}
                                   className={classNames(
                                     active ? "bg-gray-100" : "",
                                     "block px-4 py-2 text-sm text-gray-700"
